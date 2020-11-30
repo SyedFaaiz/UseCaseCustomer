3ac30bd1834e3efc62c6f67a3a2e1d86b344f6ac
faaiz rehan <faaizrehan@hotmail.com>
2020-11-30 09:58:06 -0500

    Added customer and repo class

---
+++ UseCaseCustomer/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseCaseCustomer.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Please enter your email")]
+        public string _Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        public string _Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city name")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your street address")]
+        public string Street { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Please enter your postal code")]
+        public string PostalCode { get; set; }
+    }
+}
---
+++ UseCaseCustomer/Models/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseCaseCustomer.Models
+{
+    public class Repository
+    {
+
+        private static List<Customer> _customers;
+
+        static Repository()
+        {
+            _customers = new List<Customer>();
+
+        }
+
+        public static void AddCustomer(Customer customer)
+        {
+            _customers.Add(customer);
+        }
+
+        public static IEnumerable<Customer> Customers => _customers;
+    }
+}
